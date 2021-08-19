21d48d8bf02365d6f6c4042123263a9745683878
Masum-Osman <ogkmasum@gmail.com>
2021-08-19 22:39:29 +0600

    more test cases added

--- test/discount.js
+++ test/discount.js
@@ -24,7 +24,68 @@ describe('TEST APIs', () => {
                 .get("/api/1/token")
                 .end((err, response) => {
                     response.should.not.have.status(201);
-                    // response.body.should.not.be.a('object')
+                    done();
+                })
+        })
+
+        it("It should not return JWT token", (done) => {
+            chai.request(API)
+                .post("/api/1/get_token")
+                .end((err, response) => {
+                    response.should.have.status(404);
+                    done();
+                })
+        })
+
+        it("It should return Token Not Found", (done) => {
+            let body = {
+                product_code: "12MxM",
+                user_id: 100,
+                amount: 1954
+            }
+            chai.request(API)
+                .post("/api/1/check_discount")
+                .send(body)
+                .end((err, response) => {
+                    response.should.have.status(401);
+                    response.body.should.be.a('object');
+                    done();
+                })
+        })
+
+        it("It should return Token Not Found", (done) => {
+            let body = {
+                product_code: "12MxM",
+                user_id: 100,
+                amount: 1954
+            }
+            chai.request(API)
+                .get("/api/1/check_discount")
+                .send(body)
+                .end((err, response) => {
+                    response.should.have.status(404);
+                    response.body.should.be.a('object');
+                    done();
+                })
+        })
+
+        it("It should return amount", (done) => {
+            let body = {
+                product_code: "12MM",
+                user_id: 100,
+                amount: 1954
+            }
+            let header = {
+                authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiIxIiwidXNlclR5cGUiOiJhZG1pbiIsInVzZXJuYW1lIjoiTWFzdW0gT3NtYW4iLCJwaG9uZSI6IjAxNjg2MTYzMzIzIiwidGVhbSI6ImRldiIsInJlZnJlc2hLZXkiOiIrNkVLYWNPcmgvNkVETC92VFFwdTlnPT0iLCJpYXQiOjE2MjkzNTY1OTZ9.5pH0uL6Ivk1PRrvKifwJ2afqxn74eEb6w2KeyOxJL9Q"
+            }
+            chai.request(API)
+                .post("/api/1/check_discount")
+                .send(body)
+                .set(header)
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('object');
+                    response.body.should.have.property('status');
                     done();
                 })
         })
